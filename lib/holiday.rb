bf1362fcad8bb16f4358ed8921a7711f278e2ec3
James Scalise <jamescscalise@gmail.com>
2019-08-26 20:04:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,6 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash.each {|key, value|
     if key.to_s == "winter"
-      binding.pry
       return key.values.flatten
     end
   }

876651d3c254c4db2ead3927d3bb9e7307cb43b4
James Scalise <jamescscalise@gmail.com>
2019-08-26 19:43:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each { |season, holiday|
+    puts season.to_s
 end
 
 def all_holidays_with_bbq(holiday_hash)
